26376ca28a8c050a7d5a4e4b8bf182e77598b137
Mert <raltahook07@gmail.com>
2023-01-10 22:11:05 +0300

    meta added to head

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
+import Head from 'next/head'
 import Header from '../components/Header'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
+  console.log(pageProps)
   return (
     <>
+    <Head>
+      <title>Alerjify</title>
+      <meta name="keywords" content="Alerji, Yemek, Yemek Tarifleri, Nefis Yemek Tarifleri, Gıda Hassasiyeti, Gıda, Hassasiyet, Tarifler, Diyet, Diyet Çeşitleri"/>
+      <link rel="icon" href="https://www.alerjify.com/images/favicon.ico" type="image/x-icon" />
+
+    </Head>
       <Header />
       <Component {...pageProps} />
     </>
