c6d3dbc42d694db83746db0f0d14a00f9c6e9fe6
Mert <raltahook07@gmail.com>
2023-01-10 22:15:07 +0300

    section title & layoutsection components added

---
+++ components/SectionTitle/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function index({ title }) {
+  return (
+    <div className="title-header">
+      <h3 className="title">{title}</h3>
+    </div>
+  );
+}
+
+export default index;
